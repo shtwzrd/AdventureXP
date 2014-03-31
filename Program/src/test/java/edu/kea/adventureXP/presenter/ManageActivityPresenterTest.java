386f6eca486963c7e5037bad451c17cf80b0632e
MFaarkrog <mortenfaarkrog@gmail.com>
2014-03-31 12:04:18 +0200

    ManageActivityUI and Presenter created

---
+++ Program/src/test/java/edu/kea/adventureXP/presenter/ManageActivityPresenterTest.java
@@ -0,0 +1,26 @@
+package edu.kea.adventureXP.presenter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ManageActivityPresenterTest {
+  
+  @Test
+  public void validateNameTest() {
+    ManageActivityPresenter map = new ManageActivityPresenter();
+    
+    assertTrue(map.validateName("Kim"));
+    assertTrue(!map.validateName(""));
+    assertTrue(map.validateName("123"));
+  }
+  
+  @Test
+  public void validatePriceTest() {
+    ManageActivityPresenter map = new ManageActivityPresenter();
+    
+    assertTrue(map.validatePrice(48.1));
+    assertTrue(!map.validatePrice(-48));
+    assertTrue(map.validatePrice(0));
+  }
+}
