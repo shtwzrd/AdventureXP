d1330bd92a883c228da468aad49701d264efd668
MFaarkrog <mortenfaarkrog@gmail.com>
2014-03-31 09:58:46 +0200

    Let's try again

--- Program/src/main/java/edu/kea/adventureXP/view/ManageActivityUI.java
+++ Program/src/main/java/edu/kea/adventureXP/view/ManageActivityUI.java
@@ -1,6 +1,37 @@
 package edu.kea.adventureXP.view;
 
+import java.awt.BorderLayout;
 
-public class ManageActivityUI {
+import javax.swing.JFrame;
+
+import edu.kea.adventureXP.model.Activity;
+
+/**
+ * User Interface class for creating a new activity as well as edit an old
+ * activity. The interface have fields for the activity's name, description and
+ * price as well as buttons for saving and discarding.
+ * 
+ * @see Activity
+ */
+public class ManageActivityUI extends JFrame {
   
+  /**
+   * Constructor for building the User Interface. It calls the buildUI method
+   * which builds the frame and the panels within.
+   */
+  public ManageActivityUI() {
+    buildUI();
+  }
+  
+  /**
+   * Builds the frame and the panels within.
+   */
+  public void buildUI() {
+    setTitle("Manage Activity");
+    setSize(400, 400);
+    setLayout(new BorderLayout());
+    
+    setVisible(true);
+    set
+  }
 }
---
+++ Program/src/main/java/edu/kea/adventureXP/view/UIColors.java
@@ -0,0 +1,22 @@
+package edu.kea.adventureXP.view;
+
+import java.awt.Color;
+
+/**
+ * <p>
+ * Class used to keep track of the colours used throughout the application. For
+ * example, if one wanted to change all light blues to whites, one would only
+ * have to change this in one place. Easy to maintain and highly reusable.
+ * </p>
+ *
+ */
+
+public class UIColors {
+  
+  public static final Color lightBlue  = new Color(186, 202, 218);
+  public static final Color darkBlue   = new Color(54, 96, 139);
+  public static final Color white      = new Color(255, 255, 255);
+  public static final Color lightBrown = new Color(230, 225, 217);
+  public static final Color darkBrown  = new Color(175, 170, 157);
+  
+}
