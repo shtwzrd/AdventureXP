19ef4d99085ebc2c50034d820b20b5e430cd2877
warreq <brandon.s.lucas@gmail.com>
2014-03-31 11:55:59 +0200

    Activity class persistent; fixed deps

--- Program/src/main/java/edu/kea/adventureXP/model/Activity.java
+++ Program/src/main/java/edu/kea/adventureXP/model/Activity.java
@@ -1,60 +1,88 @@
 package edu.kea.adventureXP.model;
 
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  * Class representing an Activity with a name, description and price.
  */
+@Entity
+@Table(name = "ACTIVITY")
 public class Activity {
-  
-  private String name;
-  private String description;
-  private double price;
-  
-  /**
-   * Controller for creating an Activity parsing its name, description and
-   * price.
-   * 
-   * @param name The name of the Activity
-   * @param description A description describing the Activity
-   * @param price The price of the activity (per person per hour)
-   */
-  public Activity(String name, String description, double price) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-  }
-  
-  /**
-   * Constructor for creating an Activity only parsing in the name of said
-   * Activity.
-   * 
-   * @param name The name of the Activity.
-   */
-  public Activity(String name) {
-    this(name, "", 0);
-  }
-  
-  public String getName() {
-    return name;
-  }
-  
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public String getDescription() {
-    return description;
-  }
-  
-  public void setDescription(String description) {
-    this.description = description;
-  }
-  
-  public double getPrice() {
-    return price;
-  }
-  
-  public void setPrice(double price) {
-    this.price = price;
-  }
-  
+	
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	private long id;
+
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+
+	@Column
+	private double price;
+
+	/**
+	 * Controller for creating an Activity parsing its name, description and
+	 * price.
+	 * 
+	 * @param name The name of the Activity
+	 * @param description A description describing the Activity
+	 * @param price The price of the activity (per person per hour)
+	 */
+	public Activity(String name, String description, double price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	/**
+	 * Constructor for creating an Activity only parsing in the name of said
+	 * Activity.
+	 * 
+	 * @param name The name of the Activity.
+	 */
+	public Activity(String name) {
+		this(name, "", 0);
+	}
+	
+	public Activity() {
+		
+	}
+	
+	public long getId() {
+		return this.id;
+	}
+	
+	private void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
