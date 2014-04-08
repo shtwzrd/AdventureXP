1d5ce5f1ee4245589031055f8272e1d0d002778b
warreq <brandon.s.lucas@gmail.com>
2014-04-08 09:58:55 +0200

    Fixed some things

--- Program/src/main/java/edu/kea/adventureXP/presenter/ManageInstructorPresenter.java
+++ Program/src/main/java/edu/kea/adventureXP/presenter/ManageInstructorPresenter.java
@@ -6,142 +6,159 @@ import java.util.List;
 import java.util.ArrayList;
 
 import edu.kea.adventureXP.view.ManageInstructorUI;
+import edu.kea.adventureXP.view.InstructorViewerUI;
 import edu.kea.adventureXP.model.InstructorController;
 import edu.kea.adventureXP.model.Instructor;
 
 public class ManageInstructorPresenter {
-	
-	ManageInstructorUI iui;
-	
-	
-	public ManageInstructorPresenter() {
-		
-	}
-	
-	public ManageInstructorPresenter(ManageInstructorUI iui) {
-		this.iui = iui;
-		iui.setSaveListener(new SaveButtonListener());
-		iui.setDiscardListener(new DiscardButtonListener());
-	}
 
-	public boolean validateFirstName(String name) {
-		return !name.isEmpty();
-	}
-	
-	public boolean validateLastName(String lname){
-		return !lname.isEmpty();
-	}
-	
-	public boolean validateStreetField(String street){
-		return !street.isEmpty();
-	}
-	
-	public boolean validateStreetNumField(String string) {
-		return Integer.parseInt(string) > 0;
-	}
-	public boolean validateCityField(String cityName){
-		return !cityName.isEmpty();
-	}
-	
-	public boolean validateZipField(String zipCode) {
-		return !zipCode.isEmpty();
-	}
-	
-	public boolean validatePhoneField(String string) {
-		return string.length() >= 7;
-	}
-	
-	public boolean validateEmailField(String email) {
-		if(email.contains("@") && email.contains(".") && !email.isEmpty())
-			return true;
-		else
-			return false;
-	}
+  ManageInstructorUI iui;
+  Instructor instructorToEdit;
+  private boolean isEdit;
 
-	/**
-	 * 
-	 * SaveButtonListener is a private class implementing the behavior of an ActionListener. The
-	 * class has the behavior for saving informations about a person..
-	 *
-	 */
-	
-	private class SaveButtonListener implements ActionListener {
-		
-		public void actionPerformed(ActionEvent e) {
-		     String errorMessage = "";
-		      boolean flag = true;
-		      
-		      if (!validateFirstName(iui.getFNameField())) {
-		        errorMessage += "- First name field cannot be empty.\n";
-		        flag = false;
-		      }
-		      
-		      if (!validateLastName(iui.getLNameField())) {
-		        errorMessage += "- Last name field cannot be empty.\n";
-		        flag = false;
-		      }
-		      
-		      if(!validateStreetField(iui.getStreetField())) {
-		    	  errorMessage += "Street cannot be empty.\n";
-		    	  flag = false;
-		      }
-		      
-		      if(!validateStreetNumField(iui.getStreetNumField())){
-		    	  errorMessage += "Street number cannot be empty.\n";
-		    	  flag = false;
-		      }
-		      
-		      if(!validateCityField(iui.getCityField())){
-		    	  errorMessage += "City field cannot be empty. \n";
-		    	  flag = false;
-		      }
-		      
-		      if(!validateZipField(iui.getZipField())){
-		    	  errorMessage += "Zip field cannot be empty. \n";
-		    	  flag = false;
-		      }
-		      
-		      if(!validatePhoneField(iui.getPhoneField())){
-		    	  errorMessage += "Phone number should contain 7 or more digits. \n";
-		    	  flag = false;
-		      }
-		      
-		      if(!validateEmailField(iui.getEmailField())){
-		    	  errorMessage += "Email should contain . and @ . And it should not be empty. \n";
-		    	  flag = false;
-		    	  
-		      }
-		      if (flag) {
-            List<String> phones = new ArrayList<>();
-            phones.add(iui.getPhoneField() + "");
-            Instructor instructor = new Instructor(
-                iui.getFNameField(),
-                iui.getLNameField(),
-                iui.getStreetNumField() + "",
-                iui.getStreetField(),
-                iui.getZipField(),
-                iui.getCityField(),
-                "Denmark",
-                phones,
-                iui.getEmailField());
 
-		        InstructorController.addInstructor(instructor);  
-		      }
-		      else
-		        iui.displayError(errorMessage);
-		}
-	}
-	/**
-	 * DiscardButtonListener is a private class implementing the behavior of an ActionListener. The
-     *class has the behavior for discarding the changes made on the information about a person.
-	 *
-	 */
-	private class DiscardButtonListener implements ActionListener {
-	    
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-	      iui.dispose();
-	    }
-	    
-	  }
+  public ManageInstructorPresenter() {
+    this.isEdit = false;
+    this.iui = new ManageInstructorUI();
+  }
+
+  public ManageInstructorPresenter(ManageInstructorUI iui) {
+    this.iui = iui;
+    this.isEdit = false;
+    iui.setSaveListener(new SaveButtonListener());
+    iui.setDiscardListener(new DiscardButtonListener());
+  }
+
+  public ManageInstructorPresenter(ManageInstructorUI iui,
+      Instructor instructorToEdit) {
+    this.iui = new ManageInstructorUI();
+    this.instructorToEdit = instructorToEdit;
+    this.isEdit = true;
+  }
+
+  public boolean validateFirstName(String name) {
+    return !name.isEmpty();
+  }
+
+  public boolean validateLastName(String lname){
+    return !lname.isEmpty();
+  }
+
+  public boolean validateStreetField(String street){
+    return !street.isEmpty();
+  }
+
+  public boolean validateStreetNumField(String string) {
+    return Integer.parseInt(string) > 0;
+  }
+  public boolean validateCityField(String cityName){
+    return !cityName.isEmpty();
+  }
+
+  public boolean validateZipField(String zipCode) {
+    return !zipCode.isEmpty();
+  }
+
+  public boolean validatePhoneField(String string) {
+    return string.length() >= 7;
+  }
+
+  public boolean validateEmailField(String email) {
+    if(email.contains("@") && email.contains(".") && !email.isEmpty())
+      return true;
+    else
+      return false;
+  }
+
+  /**
+   * 
+   * SaveButtonListener is a private class implementing the behavior of an ActionListener. The
+   * class has the behavior for saving informations about a person..
+   *
+   */
+
+  private class SaveButtonListener implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+      String errorMessage = "";
+      boolean flag = true;
+
+      if (!validateFirstName(iui.getFNameField())) {
+        errorMessage += "- First name field cannot be empty.\n";
+        flag = false;
+      }
+
+      if (!validateLastName(iui.getLNameField())) {
+        errorMessage += "- Last name field cannot be empty.\n";
+        flag = false;
+      }
+
+      if(!validateStreetField(iui.getStreetField())) {
+        errorMessage += "Street cannot be empty.\n";
+        flag = false;
+      }
+
+      if(!validateStreetNumField(iui.getStreetNumField())){
+        errorMessage += "Street number cannot be empty.\n";
+        flag = false;
+      }
+
+      if(!validateCityField(iui.getCityField())){
+        errorMessage += "City field cannot be empty. \n";
+        flag = false;
+      }
+
+      if(!validateZipField(iui.getZipField())){
+        errorMessage += "Zip field cannot be empty. \n";
+        flag = false;
+      }
+
+      if(!validatePhoneField(iui.getPhoneField())){
+        errorMessage += "Phone number should contain 7 or more digits. \n";
+        flag = false;
+      }
+
+      if(!validateEmailField(iui.getEmailField())){
+        errorMessage += "Email should contain . and @ . And it should not be empty. \n";
+        flag = false;
+
+      }
+      if (flag) {
+        if(isEdit) {
+          InstructorController.updateInstructor(instructorToEdit);
+        } else {
+          List<String> phones = new ArrayList<>();
+          phones.add(iui.getPhoneField() + "");
+          Instructor instructor = new Instructor(
+              iui.getFNameField(),
+              iui.getLNameField(),
+              iui.getStreetNumField() + "",
+              iui.getStreetField(),
+              iui.getZipField(),
+              iui.getCityField(),
+              "Denmark",
+              phones,
+              iui.getEmailField());
+
+          InstructorController.addInstructor(instructor);  
+        }
+      } else {
+        iui.displayError(errorMessage);
+      }
+    }
+  }
+  /**
+   * DiscardButtonListener is a private class implementing the behavior of an ActionListener. The
+   *class has the behavior for discarding the changes made on the information about a person.
+   *
+   */
+  private class DiscardButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      iui.dispose();
+    }
+
+  }
 } 
+
